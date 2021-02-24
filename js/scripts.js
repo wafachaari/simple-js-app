5bd64128377c03fed9182671dd400e40ad276d07
wafachaari <wafachaari30@gmail.com>
2021-02-24 14:38:13 +0100

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,10 @@
 
 
-  alert('Hello world');
-  var favouriteFood= `pizza`;
-  document.write(favouriteFood) ;
+let pokemonList = [
+  { name: 'Bulbasur', height: 3, types: ['grass', 'poison'] },
+  { name: 'Ivysaur', height: 1, types: ['grass', 'psychic'] },
+  { name: 'Charmander', height: 1.6, types: ['fire'] },
+ ];
+ for (var i = 0; i < pokemonList.length; i++) {
+     document.write( pokemonList[i].name + ' height:'+  pokemonList[i].height + '<br>' );
+}
