1a0e3f407e092524871fd004a862f875c10fc254
wafachaari <wafachaari30@gmail.com>
2021-02-25 12:06:24 +0100

    Update scripts.js
    If applied, this commit will have added a comment on pokemon height

--- js/scripts.js
+++ js/scripts.js
@@ -4,7 +4,13 @@ let pokemonList = [
   { name: 'Bulbasur', height: 3, types: ['grass', 'poison'] },
   { name: 'Ivysaur', height: 1, types: ['grass', 'psychic'] },
   { name: 'Charmander', height: 1.6, types: ['fire'] },
+  { name: 'Bulbasaur',  height: 0.7, type: ['grass', 'poison'] },
  ];
  for (var i = 0; i < pokemonList.length; i++) {
-     document.write( pokemonList[i].name + ' height:'+  pokemonList[i].height + '<br>' );
+     document.write( pokemonList[i].name + ' height:'+  pokemonList[i].height  );
+     //comments on pokemon height
+     if(pokemonList[i].height > 1.0){
+     document.write(' --- that\'s pretty tall' + '<br>') ;
+   }
+   else { document.write ("<br/>");    }
 }
