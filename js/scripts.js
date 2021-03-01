62c2e5959a072c0b74ba225b89073ba1d6a9edc3
wafachaari <wafachaari30@gmail.com>
2021-03-01 20:33:26 +0100

    Update scripts.js
    f applied, this commit will added the function displayPokemon

--- js/scripts.js
+++ js/scripts.js
@@ -7,7 +7,6 @@
     { name: 'Ivysaur', height: 1, types: ['grass', 'psychic'] },
     { name: 'Charmander', height: 1.6, types: ['fire'] },
     ];
-
   function add(pokemon){
     if ((typeof pokemon) == 'object'  &&  (JSON.stringify (Object.keys(pokemon) ) === JSON.stringify( [ 'name', 'height', 'types' ])))
      { pokemonList.push(pokemon);
@@ -20,22 +19,21 @@ function getAll(){
   return pokemonList;
 }
 
- return{add:add,
- getAll:getAll
-};
-})();
-
- pokemonRepository.add(  { name: 'Bulbasaur',  height: 0.7, type: ['grass', 'poison'] });
- console.log(pokemonRepository.getAll());
-
-//// IIFE function with loop to pokemonList array
-
-(function () {
- pokemonRepository.getAll().forEach(function(pokemon) {
-  document.write( pokemon.name + ' height:'+  pokemon.height  );
-  if(pokemon.height > 1.0){
-  document.write(' --- that\'s pretty tall' + '<br>') ;
+function displayPokemon() {
+getAll().forEach(function(pokemon) {
+document.write( pokemon.name + ' height:'+ pokemon.height );
+if(pokemon.height > 1.0){
+document.write(' --- that\'s pretty tall' + '<br>') ;
 }
-else { document.write ("<br/>");    }
+else { document.write ("<br/>"); }
 });
-})();
+}
+
+ return{add:add,
+ getAll:getAll,
+ displayPokemon:displayPokemon
+};
+})(); 
+ pokemonRepository.add( { name: 'Charmander', height: 1.6, types: ['fire'] });
+ console.log(pokemonRepository.getAll());
+pokemonRepository.displayPokemon();
