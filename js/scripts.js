17b14ebc04f96a7ede94399e22d497c5058b4172
wafachaari <wafachaari30@gmail.com>
2021-03-04 14:13:27 +0100

    Update scripts.js
    if applied, this commit will have added four function: loadlist(),loadDetails(), showLoadingmessage() and the function hideloadingmessage()

--- js/scripts.js
+++ js/scripts.js
@@ -2,24 +2,24 @@
 
 // IIFE pokemonRepository
  let pokemonRepository =(function(){
-  let pokemonList = [
-    { name: 'Bulbasur', height: 3, types: ['grass', 'poison'] },
-    { name: 'Ivysaur', height: 1, types: ['grass', 'psychic'] },
-    { name: 'Charmander', height: 1.6, types: ['fire'] },
-    ];
+  let pokemonList = [];
+  let apiUrl = 'https://pokeapi.co/api/v2/pokemon/?limit=10';
+//function to add new pokemon to the list
   function add(pokemon){
-    if ((typeof pokemon) == 'object'  &&  (JSON.stringify (Object.keys(pokemon) ) === JSON.stringify( [ 'name', 'height', 'types' ])))
+    if ((typeof pokemon) == 'object'  &&
+       "name" in pokemon)
      { pokemonList.push(pokemon);
      console.log('a new pokemon added');
      }
     else console.log('this is not an object');
      }
-
+//function to get the list of the pokemon
 function getAll(){
   return pokemonList;
 }
-//
-function addListItem(pokemon){let ulelement =document.querySelector(".pokemon-list");
+// Function to create button and display it on the screen
+function addListItem(pokemon){
+  let ulelement =document.querySelector(".pokemon-list");
   let listItem = document.createElement("li");
   let button = document.createElement("button");
   button.innerText = pokemon.name;
@@ -34,18 +34,70 @@ showDetails(pokemon);
 //showdetails of a pokemon when a button clicked
 function showDetails(pokemon){
 console.log(pokemon);
+pokemonRepository.loadDetails(pokemon).then(function(){
+  console.log(pokemon);
+})
+}
+//function to load pokemon list using fetch
+  function loadList() {
+    showLoadingMessage();
+    return fetch(apiUrl).then(function (response) {
+      return response.json();
+    }).then(function (json) {
+      hideLoadingMessage();
+      json.results.forEach(function (item) {
+        let pokemon = {
+          name: item.name,
+          detailsUrl: item.url
+        };
+        add(pokemon);
+
+      });
+    }).catch(function (e) {hideLoadingMessage();
+      console.error(e);
+    })
+  }
+  //Function to load more details
+function loadDetails(item){
+  showLoadingMessage();
+   let url = item.detailsUrl;
+        return fetch(url).then(function (response) {
+            return response.json();
+        }).then(function (details) { hideLoadingMessage();
+            // Now we add the details to the item
+            item.imageUrl = details.sprites.front_default;
+            item.height = details.height;
+            item.types = details.types;
+        }).catch(function (e) {hideLoadingMessage();
+            console.error(e);
+        });
 }
 
- return{add:add,
+//function to show loading message
+function showLoadingMessage(){
+ let container = document.querySelector('.container');
+ let message = document.createElement('p');
+ message.innerText = " loading message....";
+ container.appendChild(message);
+}
+//function to hide to loading message
+function hideLoadingMessage(){
+ let elementToRemove = document.querySelector('p');
+ elementToRemove.parentElement.removeChild(elementToRemove);
+}
+
+ return{
+   add:add,
  getAll:getAll,
  addListItem:addListItem,
-
+ loadList:loadList,
+ loadDetails: loadDetails
 };
 })();
- pokemonRepository.add( { name: 'Charmander', height: 1.6, types: ['fire'] });
- console.log(pokemonRepository.getAll());
 
- pokemonRepository.getAll().forEach(function (pokemon) {
 
-  pokemonRepository.addListItem(pokemon);
+ pokemonRepository.loadList().then(function() {
+   pokemonRepository.getAll().forEach(function (pokemon) {
+     pokemonRepository.addListItem(pokemon);
+});
 });
